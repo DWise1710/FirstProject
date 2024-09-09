45a60fe5470f22aaa9ff5839b3e29e52eac9c458
RC_Student_lab <RC_Student_lab@10.0.0.164>
2024-09-09 08:23:18 +0000

    Created a program to add two numbers

--- src/main/java/com/mycompany/firstproject2/FirstProject2.java
+++ src/main/java/com/mycompany/firstproject2/FirstProject2.java
@@ -11,6 +11,10 @@ package com.mycompany.firstproject2;
 public class FirstProject2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      int  a=5;
+      int b =10;
+      int c =a+b;
+      system.out.println("Sum : + c");
+       //Are we committed to the cause
     }
 }
